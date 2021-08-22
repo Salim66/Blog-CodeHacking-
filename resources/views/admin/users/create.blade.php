bf437833df47c81837324c249cdb0e5e7a291465
Salim66 <salimhasanriad8@gmail.com>
2021-08-22 17:28:09 +0600

    Laravel collective html package setup users form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,5 +3,15 @@
 @section('content')
 
     <h1>User Create</h1>
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\AdminUserController@store']) !!}
+        <div class="form-group">
+            {!! Form::label('name') !!}
+            {!! Form::text('name', null, ['class' => 'form-control', 'autocomplete' => 'off']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Add User', ['class' => 'btn btn-primary']) !!}
+        </div>
+    {!! Form::close() !!}
 
 @stop
