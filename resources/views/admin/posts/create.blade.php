34db89ffd3c4f6b75bf391bc57612565ac1c59ff
Salim66 <salimhasanriad8@gmail.com>
2021-08-23 14:49:07 +0600

    Creating post form part 1

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,6 +3,29 @@
 
 @section('content')
 
-    <h1>Post Create Page</h1>
+    <h1>Create Post</h1>
+
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\AdminPostController@store', 'files' => true]) !!}
+        <div class="form-group">
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, ['class' => 'form-control', 'autocomplete' => 'off']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('category_id', 'Category:') !!}
+            {!! Form::select('category_id', ['' => 'Options'], null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('photo_id', 'Photo:') !!}
+            {!! Form::file('photo_id', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('body', 'Description:') !!}
+            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Add Post', ['class' => 'btn btn-primary']) !!}
+        </div>
+    {!! Form::close() !!}
+
 
 @stop
