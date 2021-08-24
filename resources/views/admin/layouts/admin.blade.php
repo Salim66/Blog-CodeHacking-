7cf2c2897b2dd2c8f16592fe09f460eab3919b86
Salim66 <salimhasanriad8@gmail.com>
2021-08-24 09:27:01 +0600

    Adding the media upload dropzone plugin

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -20,8 +20,7 @@
     <link href="{{asset('admin/assets/css/libs/sb-admin-2.css')}}" rel="stylesheet">
     <link href="{{asset('admin/assets/css/libs/styles.css')}}" rel="stylesheet">
 
-    {{-- <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> --}}
+    @yield('styles')
 
 </head>
 
@@ -334,6 +333,7 @@
 <script src="{{asset('admin/assets/js/libs/sb-admin-2.js')}}"></script>
 <script src="{{asset('admin/assets/js/libs/scripts.js')}}"></script>
 
+@yield('scripts')
 
 @yield('footer')
 
--- resources/views/admin/media/create.blade.php
+++ resources/views/admin/media/create.blade.php
@@ -1,10 +1,22 @@
 @extends('admin.layouts.admin')
 
-
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/dropzone.min.css">
+@stop
 
 @section('content')
 
     <h1>Upload Media</h1>
 
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\AdminMediaContorller@store', 'class' => 'dropzone']) !!}
 
+
+    {!! Form::close() !!}
+
+
+@stop
+
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/min/dropzone.min.js"></script>
 @stop
