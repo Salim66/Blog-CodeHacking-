c024aa60ff1e41e52f90fd1d07535675442fa24e
Salim66 <salimhasanriad8@gmail.com>
2021-08-24 11:19:43 +0600

    Create comment and commentReplay model and migration setup part 1

---
+++ database/migrations/2021_08_24_110357_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('post_id')->index();
+            $table->unsignedBigInteger('is_active')->default(0)->index();
+            $table->string('author');
+            $table->string('email');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
