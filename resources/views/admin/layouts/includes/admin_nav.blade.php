58ebb28e5940f97a8414b6468f897858959b76d1
Salim66 <salimhasanriad8@gmail.com>
2021-08-26 14:17:32 +0600

    Making backend more modular

---
+++ resources/views/admin/layouts/includes/admin_nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+
+    @include('admin.layouts.includes.admin_top_nav')
+
+
+
+
+
+    @include('admin.layouts.includes.admin_side_nav')
+    <!-- /.navbar-static-side -->
+</nav>
